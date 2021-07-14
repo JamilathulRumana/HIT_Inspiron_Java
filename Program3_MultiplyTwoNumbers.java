4f059b24035f20c4da44e0be5847f432441b3228
JamilathulRumana <jr.rumana@gmail.com>
2021-07-14 15:41:54 +0530

    Delete Program3_MultiplyTwoNumbers.java

--- Program3_MultiplyTwoNumbers.java
+++
@@ -1,8 +0,0 @@
-
-public class Program3_MultiplyTwoNumbers {
-	public static void main(String[] args) {
-		int num1 = 30, num2 = 20;
-		System.out.println("The multiplication of " +num1+ "*" +num2+ " are : "+ (num1*num2));
-	}
-
-}
