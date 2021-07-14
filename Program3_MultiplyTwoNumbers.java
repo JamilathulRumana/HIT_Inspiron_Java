5d70752fd60387ce1fc3470ffcae5b469e1aa0f4
JamilathulRumana <jr.rumana@gmail.com>
2021-07-14 15:32:37 +0530

    Add files via upload

---
+++ Program3_MultiplyTwoNumbers.java
@@ -0,0 +1,8 @@
+
+public class Program3_MultiplyTwoNumbers {
+	public static void main(String[] args) {
+		int num1 = 30, num2 = 20;
+		System.out.println("The multiplication of " +num1+ "*" +num2+ " are : "+ (num1*num2));
+	}
+
+}
