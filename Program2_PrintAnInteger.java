55fb09ee3fcded07f56c8e2536fb03b056c4ed1a
JamilathulRumana <jr.rumana@gmail.com>
2021-07-14 15:27:22 +0530

    Add files via upload

---
+++ Program2_PrintAnInteger.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Program2_PrintAnInteger 
+{
+	public static void main(String[] args)
+	{
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num1 = scan.nextInt();
+		System.out.println("Hey! You have entered "+num1+ ".");
+	}
+}
