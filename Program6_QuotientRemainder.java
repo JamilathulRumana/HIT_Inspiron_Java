becbfe384da292a353486ab6d56acccd8d96b239
JamilathulRumana <jr.rumana@gmail.com>
2021-07-16 08:04:39 +0530

    Add files via upload

---
+++ Program6_QuotientRemainder.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+
+public class Program6_QuotientRemainder {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the value for dividend: ");
+		int num1 = scan.nextInt();
+		System.out.println("Enter the value for divisor: ");
+		int num2 = scan.nextInt();
+		double quotient = (double)num1/num2;
+		int reminder = num1%num2;
+		System.out.println("quotient = " +quotient);
+		System.out.println("Reminder = " +reminder);
+	}
+
+}
