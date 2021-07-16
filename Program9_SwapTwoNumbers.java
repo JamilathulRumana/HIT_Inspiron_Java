e755c7ae2c11fc9d2a3990b8621ee0d51b7ae908
JamilathulRumana <jr.rumana@gmail.com>
2021-07-16 09:07:47 +0530

    Add files via upload

---
+++ Program9_SwapTwoNumbers.java
@@ -0,0 +1,16 @@
+
+public class Program9_SwapTwoNumbers {
+	public static void main(String[] args) {
+		int num1 = 40;
+		int num2 = 10;
+		int temp;
+		
+		temp = num1;
+		num1 = num2;
+		num2 = temp;
+		
+		System.out.println("Swapped : number 1 is "+num1 +", number 2 is "+ num2);
+
+	}
+
+}
