c756edf3438ec31d87b117b5ff8d0ff6b5729a04
JamilathulRumana <jr.rumana@gmail.com>
2021-07-16 08:26:05 +0530

    Add files via upload

---
+++ Program6_ii__OddOrEven.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Program6_ii__OddOrEven {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Enter a number: ");
+		int num1 = scan.nextInt();
+		if(num1%2==0)
+		{
+			System.out.println("The Entered Number is EVEN");
+		}
+		else
+		{
+			System.out.println("The Entered Number is ODD");
+		}
+
+	}
+
+}
