d8536a864920e6340c0cf52d604c322c1cb4cc9d
JamilathulRumana <jr.rumana@gmail.com>
2021-07-16 07:50:09 +0530

    Add files via upload

---
+++ Program5_ASCIIvalue.java
@@ -0,0 +1,21 @@
+
+public class Program5_ASCIIvalue {
+	public static void main(String[] args) {
+		
+		//character to ASCII
+		char ch = 'a';
+		int i = ch;
+		System.out.println(i);
+		
+		char c1 = '$';
+		int i1 = (int)c1;
+		System.out.println(i1);
+		
+		int i2 = 169;
+		char c2 = (char)i2;
+		System.out.println(c2);
+		
+		
+	}
+
+}
